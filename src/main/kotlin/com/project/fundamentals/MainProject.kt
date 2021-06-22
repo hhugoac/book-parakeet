ec831b5a4b13a51e275600a3cf11bf866a2bc904
Hugo Alonzo <hhugoac@gmail.com>
2021-06-22 11:48:56 -0500

    Commit inicial

---
+++ src/main/kotlin/com/project/fundamentals/MainProject.kt
@@ -0,0 +1,39 @@
+package com.project.fundamentals
+
+const val SPLASH_TEXT = "                                                               ,&                                   \n" +
+        "                                        #&.                        /%                               \n" +
+        "                   %@*        *@*    @                                 &,                           \n" +
+        "             ,@                    @                                       @                        \n" +
+        "         /%                          .@                                       ,&                    \n" +
+        "     ,@                                 %&                                        %(                \n" +
+        "  #(@                                      @                                          @             \n" +
+        " .@   @                                      ,@                                          *%         \n" +
+        ".@@     %                                       @/                                           @%     \n" +
+        "  @@@    &                                         @                #@@@&*.                   @     \n" +
+        "    @@.    @                                         /@        %@/                            &@@@  \n" +
+        "     &@@    (,                                          @#  &@                                 @@@@@\n" +
+        "       @@,    @                            ,@@%,         .@@          #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n" +
+        "        %@@    ,/                    /@(                @@@@@@@* ,@@@@@@@@@@@@@&/.                  \n" +
+        "          @@(    @              #@.                    &@@@@@@@@@*                                  \n" +
+        "           /@@     &       ,@(              #@@@@@@@@@@@@@@@&.                                      \n" +
+        "             @@@    /*,@%            %@@@@@@@@%                                                     \n" +
+        "              .@@.   @          #@@@@@@@*                                                           \n" +
+        "                @@@  @      @@@@@&.                                                                 \n" +
+        "                 *@@&% (@@@@                                                                        \n" +
+        "                   @@@#                                                                             "
+
+fun main () {
+    println(SPLASH_TEXT)
+    println("Presione una tecla para comenzar")
+    val age = readLine()?.toInt()
+
+    TODO("Login")
+
+    TODO("Main menu")
+
+    TODO("Book details")
+
+    TODO("Add book")
+}
+
+
