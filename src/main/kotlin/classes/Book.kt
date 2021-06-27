6b042f593b8d3a9dede4a4802f724377d1cfc9a0
AndGut49 <andie.gtz00@gmail.com>
2021-06-27 17:19:47 -0500

    creando Clase Usuario y validando la clase Libro

--- src/main/kotlin/classes/Book.kt
+++ src/main/kotlin/classes/Book.kt
@@ -21,4 +21,31 @@ class Book {
             println("El libro se llama $title de $author." +
                     "Contiene $pages y es del genero $gender. Se trata de $summary")
         }
+
+        fun validateFields() : Boolean {
+            if(title.length > 40){
+                println("El campo titulo no puede tener mas de 40 caracteres")
+                return false
+            }
+            else if (author.length > 30){
+                println("El campo autor no puede tener mas de 30 caracteres")
+                return false
+            }
+            else if(gender.length > 15){
+                println("El campo genero no puede tener mas de 15 caracteres")
+                return false
+            }
+            else if(!pages is Int){
+                println("El campo pages debe ser del tipo Int")
+            }
+            else if(summary.length > 100){
+                println("El campo resumen no puede contener mas de 100 caracteres")
+            }
+            else if(!title is String || !author is String || !gender is String || !summary is String){
+                println("El campo titulo, autor, genero o resumen debe ser del tipo String")
+            }
+        }
+
+
+
     }
--- src/main/kotlin/classes/User.kt
+++ src/main/kotlin/classes/User.kt
@@ -5,16 +5,17 @@ import java.util.*
 class User {
         var name:String
         var lastName:String
-        var city:String
         var birthday:Date
         var email:String
+        var phone: String
         var password: String
 
-        constructor(name:String, lastName:String, city:String, birthday:Date,email:String,password:String){
+        constructor(name:String, lastName:String, birthday:Date,email:String,phone:String,password:String){
             this.name=name
             this.lastName=lastName
             this.birthday=birthday
             this.email=email
+            this.phone=phone
             this.password=password
         }
 
