98da3c44447852f6724b83e28bb77edc17c2b207
AndGut49 <andie.gtz00@gmail.com>
2021-07-03 22:51:19 -0500

    Actualización Inicio Sesion

--- src/main/kotlin/classes/Login.kt
+++ src/main/kotlin/classes/Login.kt
@@ -1,12 +1,21 @@
 package classes
 
-class Login {
+class Login (email: String,password: String){
+    var correo=email
+    var contrasena=password
 
-    val usuario = ""
-    val correo = ""
-    val contrasena = ""
+    fun validateLogin():Boolean{
+        if(correo == "admin" && contrasena == "123"){
+            println("Inicio de sesión exitoso! Bienvenido")
+            return true
+        }else if (correo.isEmpty() || correo != "admin" || contrasena.isEmpty() || contrasena != "123"){
+            println("Correo o contraseña erroneo, favor de verificar")
+            return false
+        }
+        return false
+    }
 
     fun accesoContraseña() = if (contrasena.isEmpty()) true else false
     fun accesoCorreo() = if (correo.isEmpty()) true else false
-    fun accesoUsuario() = if (usuario.isEmpty()) true else false
+
 }
---
+++ src/main/kotlin/project/Main.kt
@@ -0,0 +1,44 @@
+package project
+import classes.Login
+import kotlin.math.log
+
+
+fun main() {
+    Menu()
+    var option=validarOption(readLine()?.toInt())
+    when(option){
+        1 -> {
+            print("Ingresa correo:")
+            val email= readLine()?.toString()
+            print("Ingresa contraseña:")
+            val password = readLine()?.toString()
+            var login = Login("$email","$password")
+            login.validateLogin()
+        }
+    }
+}
+
+fun Menu(){
+    println("##########TINDER DE LIBROS#########")
+    println("Presione una tecla para comenzar")
+    println("1. Iniciar sesión")
+    println("2. Registrarse")
+    println("3. Salir")
+    print("Seleccione una opcion:")
+}
+
+fun validarOption(option: Int?): Int{
+    when(option){
+        1 -> return 1
+        2 -> return 2
+        3 -> return 3
+        else -> {
+            println("Opcion no valida")
+            Menu()
+            validarOption(readLine()?.toInt())
+            return -1
+        }
+    }
+}
+
+
